bdf713d8a2b2047fc59f066790ecee7e30d09506
Jeff <the.smith.dev@gmail.com>
2022-11-20 16:33:55 -0600

    Adding tests for backstage and legendary items

---
+++ csharpcore-Verify.xunit/GildedRoseTests/ItemTests/BackstageItemTests.cs
@@ -0,0 +1,78 @@
+﻿using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests.ItemTests
+{
+    public class BackstageItemTests : TestingBase
+    {
+        private GildedRose _target;
+
+        [Theory]
+        //regular increase
+        [InlineData(10, 20, 21)]
+        [InlineData(5, 30, 31)]
+        //50 is quality ceiling
+        [InlineData(5, 50, 50)]
+        //past sell by doubles
+        [InlineData(-1, 30, 32)]
+        //past sell by maintains max
+        [InlineData(0, 50, 50)]
+        [InlineData(-1, 50, 50)]
+        public void ItemUpdateQuality_Brie(int sellIn, int quality, int expected)
+        {
+            var list = itemFake
+                .RuleFor(x => x.Name, fake => "Aged Brie")
+                .RuleFor(x => x.Quality, fake => quality)
+                .RuleFor(x => x.SellIn, fake => sellIn)
+                .Generate(1);
+
+            //Backstage passes to a TAFKAL80ETC concert
+
+            _target = new GildedRose(list);
+            _target.UpdateQuality();
+
+            Assert.Equal(expected, list[0].Quality);
+        }
+
+        [Theory]
+        //regular increase
+        [InlineData(10, 20, 22)]
+        [InlineData(5, 30, 33)]
+        //50 is quality ceiling
+        [InlineData(5, 48, 50)]
+        //past sell by
+        [InlineData(0, 50, 0)]
+        public void ItemUpdateQuality_ConcertTickets(int sellIn, int quality, int expected)
+        {
+            var list = itemFake
+                .RuleFor(x => x.Name, fake => "Backstage passes to a TAFKAL80ETC concert")
+                .RuleFor(x => x.Quality, fake => quality)
+                .RuleFor(x => x.SellIn, fake => sellIn)
+                .Generate(1);
+
+            //Backstage passes to a TAFKAL80ETC concert
+
+            _target = new GildedRose(list);
+            _target.UpdateQuality();
+
+            Assert.Equal(expected, list[0].Quality);
+        }
+
+        [Fact]
+        public void ItemUpdateSellIn()
+        {
+            var list = itemFake
+                .RuleFor(x => x.Name, fake => "Aged Brie")
+                .RuleFor(x => x.Quality, fake => 10)
+                .RuleFor(x => x.SellIn, fake => 10)
+                .Generate(1);
+            var expectedSellin = list[0].SellIn - 1;
+
+            _target = new GildedRose(list);
+            _target.UpdateQuality();
+
+            Assert.Equal(expectedSellin, list[0].SellIn);
+        }
+
+    }
+}
---
+++ csharpcore-Verify.xunit/GildedRoseTests/ItemTests/LegendaryItemTests.cs
@@ -0,0 +1,56 @@
+﻿using GildedRoseKata;
+using Xunit;
+
+namespace GildedRoseTests.ItemTests
+{
+    public class LegendaryItemTests : TestingBase
+    {
+        private GildedRose _target;
+
+        [Fact]
+        public void ItemUpdateQuality()
+        {
+            var list = itemFake
+                .RuleFor(x => x.Name, fake => "Sulfuras, Hand of Ragnaros")
+                .RuleFor(x => x.Quality, fake => 80)
+                .Generate(1);
+            var expectedQuality = list[0].Quality;
+
+            _target = new GildedRose(list);
+            _target.UpdateQuality();
+
+            Assert.Equal(expectedQuality, list[0].Quality);
+        }
+
+        [Fact]
+        public void ItemUpdateSellIn()
+        {
+            var list = itemFake
+                .RuleFor(x => x.Name, fake => "Sulfuras, Hand of Ragnaros")
+                .RuleFor(x => x.Quality, fake => 80)
+                .Generate(1);
+            var expectedSellin = list[0].SellIn;
+
+            _target = new GildedRose(list);
+            _target.UpdateQuality();
+
+            Assert.Equal(expectedSellin, list[0].SellIn);
+        }
+
+        [Fact]
+        public void ItemUpdateQuality_PastSellin()
+        {
+            var list = itemFake
+                .RuleFor(x => x.Name, fake => "Sulfuras, Hand of Ragnaros")
+                .RuleFor(x => x.Quality, fake => 80)
+                .RuleFor(x => x.SellIn, fake => 0).Generate(1);
+            var expectedQuality = list[0].Quality;
+
+            _target = new GildedRose(list);
+            _target.UpdateQuality();
+
+            Assert.Equal(expectedQuality, list[0].Quality);
+        }
+
+    }
+}
