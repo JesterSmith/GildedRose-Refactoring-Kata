dc387ce96194725a7899fbe3d52d32b0a40a89a1
Jeff <the.smith.dev@gmail.com>
2022-11-20 17:34:45 -0600

    Updating testing base

--- csharpcore-Verify.xunit/GildedRoseTests/TestingBase.cs
+++ csharpcore-Verify.xunit/GildedRoseTests/TestingBase.cs
@@ -7,13 +7,24 @@ namespace GildedRoseTests
     public class TestingBase
     {
         public Faker<Item> itemFake;
+        public Faker<Item> legendaryFake;
+        public Faker<Item> conjuredFake;
 
 
         public TestingBase()
         {
             itemFake = new AutoFaker<Item>()
-                  .RuleFor(x => x.Quality, fake => fake.Random.Int(10, 20))
-                  .RuleFor(x => x.SellIn, fake => fake.Random.Int(10, 20));
+                .RuleFor(x => x.Quality, fake => fake.Random.Int(10, 20))
+                .RuleFor(x => x.SellIn, fake => fake.Random.Int(10, 20));
+
+            legendaryFake = new AutoFaker<Item>()
+                .RuleFor(x => x.Name, fake => "Sulfuras, Hand of Ragnaros")
+                .RuleFor(x => x.Quality, fake => 80);
+
+            conjuredFake = new Faker<Item>()
+                .RuleFor(x => x.Name, fake => "Conjured Mana Cake")
+                .RuleFor(x => x.SellIn, fake => 10)
+                .RuleFor(x => x.Quality, fake => 20);
         }
     }
 }
